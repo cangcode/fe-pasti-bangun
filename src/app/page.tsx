bbbafd3f9e2345ee656c5add2bc28eca61a1e004
cangcode <cangcodes@gmail.com>
2025-06-03 02:00:50 +0800

    feat : add env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function AlarmPage() {
 
   useEffect(() => {
     // Ganti dengan URL backend kamu
-    fetch('http://localhost:5000/api/kode')
+    fetch(process.env.NEXT_PUBLIC_API_URL+'/api/kode')
       .then(res => res.json())
       .then(data => setKode(data.kode))
       .catch(() => setStatus('Gagal ambil kode'));
@@ -19,7 +19,7 @@ export default function AlarmPage() {
     setStatus('Memeriksa kode...');
 
     try {
-      const res = await fetch('http://localhost:5000/api/cek-kode', {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_URL+'/api/cek-kode', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ kode: inputKode }),
